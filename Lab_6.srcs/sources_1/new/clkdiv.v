707698e7c2894ccc4b980e604a5f1b3efdab6f36
Samarth Kabbur <samarthkabbur2016@gmail.com>
2024-11-19 18:00:24 -0600

    test

---
+++ Lab_6.srcs/sources_1/new/clkdiv.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/19/2024 05:41:11 PM
+// Design Name: 
+// Module Name: stopwatch_main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clkdiv(
+
+    );
+endmodule
---
+++ Lab_6.srcs/sources_1/new/stopwatch_main.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/19/2024 05:41:11 PM
+// Design Name: 
+// Module Name: stopwatch_main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module stopwatch_main(
+    input clk
+
+
+
+    );
+
+clkdiv c1();
+mode1 c2();
+mode2 c3();
+mode3 c4();
+mode4 c5();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+endmodule
